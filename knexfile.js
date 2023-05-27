eb901f13e223b7ff5e6ca100d37d88ee16fd73b4
garrettomi <omigarrett@gmail.com>
2023-05-27 16:26:34 +0900

    Fix: add query parameter of ?ssl=true to DB URL in knexfile

--- knexfile.js
+++ knexfile.js
@@ -23,9 +23,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: {
-      database: process.env.DATABASE_URL,
-    },
+    connection: process.env.DATABASE_URL + "?ssl=true",
     migrations: {
       directory: "./db/migrations",
     },
